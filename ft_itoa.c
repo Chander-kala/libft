24b4dfaa85234b1407940302a76710437f3d4a28
Chander-kala <89724783+Chander-kala@users.noreply.github.com>
2022-03-19 18:33:36 +1030

    Add files via upload

---
+++ ft_itoa.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ckala <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/19 17:27:04 by ckala             #+#    #+#             */
+/*   Updated: 2022/03/19 17:33:31 by ckala            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static unsigned int	ft_numsize(int n)
+{
+	unsigned int	len;
+
+	if (n == 0)
+		return (1);
+	len = 0;
+	if (n < 0)
+		len += 1;
+	while (n != 0)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	char			*str;
+	unsigned int	num;
+	unsigned int	len;
+
+	len = ft_numsize(n);
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+	if (n < 0)
+	{
+		str[0] = '-';
+		num = -n;
+	}
+	else
+		num = n;
+	if (num == 0)
+		str[0] = '0';
+	str[len] = '\0';
+	while (num != 0)
+	{
+		str[len - 1] = (num % 10) + '0';
+		num = num / 10;
+		len--;
+	}
+	return (str);
+}
+/*Converts an interger data type to a string data type.*/
